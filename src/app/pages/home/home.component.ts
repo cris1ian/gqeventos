8cb411f2b2218f4a768704f32a1127ec3a36d404
AgustinCocconi <agustin.cocconi@gmail.com>
2019-11-22 19:06:41 -0300

    Home inicial

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
+})
+
+@NgModule({
+ exports: [
+   MatButtonModule
+ ]
 })
 export class HomeComponent implements OnInit {
 
