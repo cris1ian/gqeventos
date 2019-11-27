9973bff991b36fe66fc496f4e98abd6ccb1eac71
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2019-11-27 15:16:53 -0300

    casi terminada la galeria admin

--- src/app/pages/galerias-admin/galerias-admin.component.ts
+++ src/app/pages/galerias-admin/galerias-admin.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./galerias-admin.component.scss']
 })
 export class GaleriasAdminComponent implements OnInit {
-  photos: any[] = [,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,];
+  photos: any[] = [, , , , , , , , , , , ,];
 
   constructor() { }
 
