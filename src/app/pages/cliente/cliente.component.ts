a98ad13894af0c225fa9cbf356fe2881b66b022e
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2019-12-02 18:10:14 -0300

    redimencione los headers

--- src/app/pages/cliente/cliente.component.ts
+++ src/app/pages/cliente/cliente.component.ts
@@ -2,32 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { Photo } from 'src/app/models/photo.model';
 import { responsePhoto1 } from 'src/app/models/backend-photos';
 
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselConfigComponent } from '../carousel-config/carousel-config.component';
+
+
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
-  styleUrls: ['./cliente.component.scss']
+  styleUrls: ['./cliente.component.scss'],
+
+  providers: [NgbModalConfig, NgbModal]
 })
 export class ClienteComponent implements OnInit {
   photos: Photo[];
   selected: number;
 
-  constructor() { }
-  
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    // config.backdrop = 'static';
+    config.centered = true;
+    // config.size = "xl";
+    config.scrollable = false;
+
+    config.windowClass = "windowOfModal";
+  }
+
+  open() {
+    this.modalService.open(CarouselConfigComponent);
+  }
+
   ngOnInit() {
     this.photos = responsePhoto1;
     this.selected = this.photos.filter(element => element.isSelected).length;
     console.log(this.photos);
-}
+  }
 
-onClick(i: number) {
+  onClick(i: number) {
     this.photos[i].isSelected = !this.photos[i].isSelected;
     this.selected = this.photos.filter(element => element.isSelected).length;
     console.log(`Click en la foto ${i}: check = ${this.photos[i].isSelected}`);
-}
+  }
 
-onEnlarge(i: number) {
-    // this.photos[i].isSelected = !this.photos[i].isSelected;
-    console.log(`Agrandar foto ${i}`);
-}    
+
 
 }
