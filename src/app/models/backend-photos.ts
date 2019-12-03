a63fccffe52b02903247bbfc84afe69306dd72ef
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2019-12-03 00:51:28 -0300

    pelenadome con el modal

--- src/app/models/backend-photos.ts
+++ src/app/models/backend-photos.ts
@@ -14,7 +14,23 @@ export const responsePhoto1 = [
     { isSelected: false, fileName: 'img.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (5).jpg", userId: 43, id: 13 },
     { isSelected: false, fileName: 'img.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (6).jpg", userId: 43, id: 14 },
     { isSelected: false, fileName: 'img.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (7).jpg", userId: 43, id: 15 },
-    { isSelected: false, fileName: 'img.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (8).jpg", userId: 43, id: 16 }
+    { isSelected: false, fileName: 'img.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (8).jpg", userId: 43, id: 16 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (1).jpg", userId: 43, id: 1 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (2).jpg", userId: 43, id: 2 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (3).jpg", userId: 43, id: 3 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (4).jpg", userId: 43, id: 4 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (5).jpg", userId: 43, id: 5 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (6).jpg", userId: 43, id: 6 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (7).jpg", userId: 43, id: 7 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (8).jpg", userId: 43, id: 8 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (1).jpg", userId: 43, id: 9 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (2).jpg", userId: 43, id: 10 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (3).jpg", userId: 43, id: 11 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (4).jpg", userId: 43, id: 12 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (5).jpg", userId: 43, id: 13 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (6).jpg", userId: 43, id: 14 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (7).jpg", userId: 43, id: 15 },
+    { isSelected: false, fileName: 'img01.jpg', picture: "../../../assets/Fotos/galeria-porfiolio/img- (8).jpg", userId: 43, id: 16 }
 ]
 
 export const responsePhoto2 = [
