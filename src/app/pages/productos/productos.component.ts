654b173491606463905de933ccc42543608bf77d
Agustin Cocconi <agustin.cocconi@gmail.com>
2019-12-04 14:30:53 -0300

    carrusel productos + estilos

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselConfigComponent } from '../carousel-config/carousel-config.component';
+
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.scss']
+  styleUrls: ['./productos.component.scss'],
+
+  providers: [NgbModalConfig, NgbModal]
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    // config.backdrop = 'static';
+    config.centered = true;
+    // config.size = "xl";
+    config.scrollable = false;
+
+    config.windowClass = "windowOfModal";
+     }
+
+  open() {
+    this.modalService.open(CarouselConfigComponent);
+  }
 
   ngOnInit() {
   }
