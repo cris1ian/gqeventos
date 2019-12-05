f6922a5d6e3b218a8d8b5bcb401eb981b544fc67
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2019-12-05 01:35:42 -0300

    con el modal de borrar implementado

--- src/app/pages/galerias-admin/galerias-admin.component.ts
+++ src/app/pages/galerias-admin/galerias-admin.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from 'src/app/models/client.model';
 import { responseClient } from 'src/app/models/backend-client';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-galerias-admin',
   templateUrl: './galerias-admin.component.html',
-  styleUrls: ['./galerias-admin.component.scss']
+  styleUrls: ['./galerias-admin.component.scss'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class GaleriasAdminComponent implements OnInit {
   clients: Client[];
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    // config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit() {
     this.clients = responseClient;
+  }
+
+  open(content) {
+    this.modalService.open(content);
   }
 
 }
