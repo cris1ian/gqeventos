01614dbc9935ef73062891c02361efa6333ca603
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2019-12-11 13:56:02 -0300

    mensaje de error en los formulario de contacto

--- src/app/pages/contactanos/contactanos.component.ts
+++ src/app/pages/contactanos/contactanos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { format } from 'url';
 
 @Component({
     selector: 'app-contactanos',
@@ -11,15 +12,20 @@ export class ContactanosComponent implements OnInit {
     email: string;
     telefono: string;
     mensaje: string;
+    showError: boolean;
+    showOk: boolean;
+
 
     constructor(
         private userService: UserService
     ) { }
 
     ngOnInit() {
+        this.showError = false;
+        this.showOk = false;
     }
 
-    contactMe() {
+    contactMe(form) {
         this.userService.contactMe(
             {
                 name: this.name,
@@ -29,13 +35,36 @@ export class ContactanosComponent implements OnInit {
             }
         ).subscribe(
             (resp: any) => {
-                if (resp.status == 0) console.log("Mostrar un alert confirmando mensaje enviado")
-                else console.log("Mostrar un alert diciendo que se produjo un error y que se intente más tarde")
+                if (resp.status == 0) {
+                    console.log(" mensaje enviado");
+                    this.showOk = true;
+                    this.showError = false;
+                    form.reset();
+
+
+                }
+                else {
+                    console.log(" se produjo un error , intente más tarde");
+                    this.showOk = false;
+                    this.showError = true;
+
+                }
             },
-            err => console.log("Mostrar un alert diciendo que se produjo un error y que se intente más tarde")
+            error => {
+                console.log(" se produjo un error , intente más tarde");
+                this.showOk = false;
+                this.showError = true;
+
+            }
         );
 
 
+
+    }
+
+    resetMessage() {
+        this.showError = false;
+        this.showOk = false;
 
     }
 
