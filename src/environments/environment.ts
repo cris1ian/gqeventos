dc752b5a68818dfc532c0523a0245e47023294ba
Agustin Cocconi <agustin.cocconi@gmail.com>
2019-12-20 15:33:32 -0300

    url bucket imagenes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   // WS_URL: 'http://localhost:3001/api/v1',
   WS_URL: "http://ec2-18-231-125-85.sa-east-1.compute.amazonaws.com:3001/api/v1",
-  S3_URL: "http://gqclientes.s3.sa-east-1.amazonaws.com"
+  S3_URL: "http://gq-eventos.s3.sa-east-1.amazonaws.com"
 };
 
 /*
